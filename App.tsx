a4694fe6de960805e40101578d0209e379c1f7d3
Valtteri Rehu <90039969+Valde00@users.noreply.github.com>
2026-01-21 14:53:05 +0200

    a

---
+++ App.tsx
@@ -0,0 +1,62 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Text>Press to open modal</Text>
+      </Pressable>
+
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType='slide'
+      >
+        <View style={styles.modal}>
+          <View style={styles.content}>
+            <Text style={styles.modalText}>This is a modal.</Text>
+            <Pressable style={styles.cancelButton} onPress={() => setModalVisible(false)}>
+              <Text>Cancel</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  content: {
+    backgroundColor: 'white',
+    padding: 20,
+    width: 300,
+    height: 200,
+    justifyContent: 'space-between',
+  },
+  modalText: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  cancelButton: {
+    fontSize:16,
+    alignSelf: 'flex-end',
+  },
+});
